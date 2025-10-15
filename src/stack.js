9bd3a080fa0ca4fdcc0e006ed06ceeb6f280f428
Alina <alina.stavs@gmail.com>
2025-10-15 23:09:08 +0300

    [feat:] stack complete

--- src/stack.js
+++ src/stack.js
@@ -13,18 +13,25 @@ const { NotImplementedError } = require("../extensions/index.js");
  *
  */
 module.exports = class Stack {
-  push(/* element */) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
-  }
+    constructor() {
+        this.items = [];
+    }
 
-  pop() {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
-  }
+    push(element) {
+        this.items.push(element);
+    }
 
-  peek() {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
-  }
+    pop() {
+        if (this.items.length === 0) {
+            return undefined;
+        }
+        return this.items.pop();
+    }
+
+    peek() {
+        if (this.items.length === 0) {
+            return undefined;
+        }
+        return this.items[this.items.length - 1];
+    }
 };
